f22703c74662e837c022c1622ed730e2d08759c9
Zach <zschmidt@uoregon.edu>
2016-01-16 04:35:53 -0500

    Bi directional communication

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ io.on('connection', function(socket){
     console.log('User disconnected');
   });
   socket.on('chat message', function(msg){
-    console.log('message: '+msg);
+    io.emit('chat message', msg);
+    console.log('Got message: '+msg);
   });
 });
 
