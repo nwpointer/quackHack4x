3e5f5226f699ea442aa582f0bf214e7e64757113
Zach <zschmidt@uoregon.edu>
2016-01-16 04:31:17 -0500

    Functioning chat client!

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('User disconnected');
   });
+  socket.on('chat message', function(msg){
+    console.log('message: '+msg);
+  });
 });
 
 http.listen(3000, function(){
