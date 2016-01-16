8a92c78504e42ab226b334046aeaa0c80de55ce2
Zach Schmidt <zschmidt@uoregon.edu>
2016-01-16 12:19:57 -0800

    I guess you have to serve each file individually...

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/scripts/socketCommunication.js');
 });
 
 io.on('connection', function(socket){
