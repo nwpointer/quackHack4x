abad7243ec778cb33969b327cb74fed120d43e4d
Zach Schmidt <zschmidt@uoregon.edu>
2016-01-16 13:27:12 -0800

    Trying to get a single event handler

--- server/index.js
+++ server/index.js
@@ -11,26 +11,37 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  var onevent = socket.onevent;
+  socket.onevent = function (packet) {
+      var args = packet.data || [];
+      onevent.call (this, packet);    // original call
+      packet.data = ["*"].concat(args);
+      onevent.call(this, packet);      // additional call to catch-all
+  };
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('User disconnected');
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('Got message: '+msg);
+  socket.on("*",function(event,data) {
+      console.log("Event: "+event);
+      console.log("Data: "+data);
   });
-  socket.on('turretPlacement', function(msg){
-    io.emit('turretPlacement', msg);
-    console.log('Got turretPlacement: '+msg);
-  });
-  socket.on('hit', function(msg){
-    io.emit('hit', msg);
-    console.log('Got hit!');
-  });
-  socket.on('creeperLocations', function(msg){
-    io.emit('creeperLocations', msg);
-    console.log('Got creeperLocations: '+msg);
-  });
+  // socket.on('chat message', function(msg){
+  //   io.emit('chat message', msg);
+  //   console.log('Got message: '+msg);
+  // });
+  // socket.on('turretPlacement', function(msg){
+  //   io.emit('turretPlacement', msg);
+  //   console.log('Got turretPlacement: '+msg);
+  // });
+  // socket.on('hit', function(msg){
+  //   io.emit('hit', msg);
+  //   console.log('Got hit!');
+  // });
+  // socket.on('creeperLocations', function(msg){
+  //   io.emit('creeperLocations', msg);
+  //   console.log('Got creeperLocations: '+msg);
+  // });
 });
 
 http.listen(80, function(){
