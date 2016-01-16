ebfc3c671542ffcf538f6fb9a3512c2213194cce
Zach <zschmidt@uoregon.edu>
2016-01-16 04:23:54 -0500

    Functioning connection logging

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('User disconnected');
+  });
 });
 
 http.listen(3000, function(){
