412bb1bd0486e75941f5829eb6508095826a8e09
Jonathan Grant <jonathanallengrant@gmail.com>
2016-01-16 01:15:32 -0800

    Shits dumb, I should sleep soon

---
+++ react/index.js
@@ -0,0 +1,56 @@
+var BottomBar = React.createClass({
+	getInitialState: function(){
+  	return {
+    	numGold: 100,
+      trtDisabled: false
+    }
+  },
+  
+  componentDidMount: function() {
+  	setTimeout(this.incrementGold,1000);
+  },
+  
+  incrementGold: function (){
+  	this.setState({
+    	numGold: this.state.numGold + 1
+    });
+    this.goldChanged();
+    setTimeout(this.incrementGold,1000);
+  },
+  
+  placeTurret: function() {
+  	if (this.state.numGold >= 50) {
+      this.setState({
+        numGold: this.state.numGold - 50
+      })
+      this.goldChanged();
+    }
+    this.goldChanged();
+  },
+  
+  goldChanged: function() {
+    if (this.state.numGold >= 50) {
+      this.setState({
+    		trtDisabled: false
+      });
+    } else {
+	    this.setState({
+    		trtDisabled: true
+      });
+    }
+  },
+  
+  render: function() {
+  	return(
+    	<div> {this.state.numGold} Gold 
+      <br/>
+      <button onClick={this.placeTurret} disabled={this.state.trtDisabled}> Place Turret</button>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+	<BottomBar />,
+  document.getElementById('container')
+);
