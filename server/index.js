de21fc936bd4bfcdc66b6e934783dccd5461930d
Zach Schmidt <zschmidt@uoregon.edu>
2016-01-16 12:25:56 -0800

    Take 3

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use('/scripts', express.static('scripts'));
+app.use(express.static(__dirname + '/'));
 
 
 app.get('/', function(req, res){
